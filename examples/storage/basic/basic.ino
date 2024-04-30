4177ebd0cb9d09663bf362a384de3129b88b4d0d
Andrew Dunstan <a_dunstan@hotmail.com>
2024-04-30 17:28:21 +1000

    basic storage example: minor/cosmetic fixes

--- examples/storage/basic/basic.ino
+++ examples/storage/basic/basic.ino
@@ -1,3 +1,10 @@
+/* basic.ino: Searches for a USB Mass Storage device, attempts to mount a
+ * FAT12, FAT16, FAT32 or EXFAT partition and recursively lists all directories/files.
+ * All attached drives, their LUNs and the partitions on them will be searched
+ * sequentially until a FAT partition is found.
+ */
+
+
 #include <string>
 #include "teensy4_usb.h"
 
@@ -5,16 +12,15 @@
 #include "usb_drivers.h"
 
 DMAMEM TeensyUSBHost2 usb;
-
 static USB_FAT_Volume USBVol;
 
-// wrapper to simplify printing DateTimeFields
+// wrapper to print DateTimeFields
 class PrintableDateTimeFields : public DateTimeFields, public Printable {
 public:
   virtual size_t printTo(Print& p) const {
     static const char* mlabels[] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec","???"};
     size_t mindex = mon < 12 ? mon : 12;
-    return p.printf("%02u:%02u:%02u %u %s %04u", hour, min, sec, mday, mlabels[mindex], year+1900);
+    return p.printf("%02u:%02u:%02u %s %u %04u", hour, min, sec, mlabels[mindex], mday, year+1900);
   }
   PrintableDateTimeFields() {
     sec=0, min=0, hour=0, wday=0, mday=1, mon=0, year=70;
@@ -22,25 +28,25 @@ public:
 };
 
 void recurse_dir_print(String base, File &f, Print &p) {
-  if (f && f.isDirectory()) {
-    p.print("DIRECTORY: ");p.println(base);
-    // parse all files then directories
-    File n;
-    while ((n = f.openNextFile()) == true) {
-      if (n.isDirectory() == false) {
-        PrintableDateTimeFields modified;
-        n.getModifyTime(modified);
-        // print fields in this order because it keeps things better aligned
-        p.print("Modified: "); p.print(modified); p.print('\t');
-        p.print("Size: "); p.print(n.size()); p.print('\t');
-        p.print("Name: "); p.println(n.name());
-      }
+  p.print("DIRECTORY: ");p.println(base);
+  // display all files in the directory
+  File n;
+  while ((n = f.openNextFile())) {
+    if (!n.isDirectory()) {
+      PrintableDateTimeFields dt;
+      n.getModifyTime(dt);
+      // print fields in this order because it keeps things better aligned
+      p.print("Modified: "); p.print(dt); p.print('\t');
+      p.print("Size: "); p.print(n.size()); p.print('\t');
+      p.print("Name: "); p.print(n.name());
+      p.println();
     }
-    f.rewindDirectory();
-    while ((n = f.openNextFile()) == true) {
-      if (n.isDirectory() == true)
-        recurse_dir_print(base + n.name() + '/', n, p);
-    }
+  }
+  // now parse into each sub-directory
+  f.rewindDirectory();
+  while ((n = f.openNextFile())) {
+    if (n.isDirectory())
+      recurse_dir_print(base + n.name() + '/', n, p);
   }
 }
 
@@ -61,14 +67,12 @@ void setup() {
 
 void loop() {
   if (Serial.read() == '\n') {
-    int retry = 5;
-    do {
-      if (USBVol.mount() == true)
-        break;
-    } while (--retry > 0);
-    if (retry == 0) {
-      Serial.println("Failed to find a USB drive with a FAT partition, press return to try again");
-      return;
+    for (int retry=0; USBVol.mount()==false; retry++) {
+      if (retry >= 5) {
+        Serial.println("Failed to find a USB drive with a FAT partition, press return to try again");
+        return;
+      }
+      delay(100);
     }
     
     Serial.print("Found a ");
@@ -80,12 +84,14 @@ void loop() {
       default: Serial.print("FAT?? "); break;
     }
     Serial.println("partition.");
-    Serial.print("Checking free space... "); USBVol.usedSize(); Serial.println("Done."); // value will be cached
-    Serial.print("Bytes free: "); Serial.print(USBVol.totalSize() - USBVol.usedSize());
-    Serial.print(" of "); Serial.println(USBVol.totalSize());
+    Serial.print("Checking free space... ");
+    uint64_t freeSpace = USBVol.totalSize() - USBVol.usedSize(); Serial.println("Done.");
+    Serial.print("Bytes free: "); Serial.print(freeSpace);
+    Serial.print(" of "); Serial.print(USBVol.totalSize());
+    Serial.print(" ("); Serial.print(freeSpace * 100 / USBVol.totalSize()); Serial.println("%)");
 
     File root = USBVol.open("/");
-    if (root) {
+    if (root && root.isDirectory()) {
       Serial.println("--- FILE LISTING BEGIN ---");
       recurse_dir_print("/", root, Serial);
       Serial.println("--- FILE LISTING COMPLETE ---");
