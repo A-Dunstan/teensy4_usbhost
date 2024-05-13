5e10ab8673f741dd30c1b422d2a1028815459e56
Andrew Dunstan <a_dunstan@hotmail.com>
2024-05-13 10:15:13 +1000

    cosmetic changes to storage/mtp example

--- examples/storage/mtp/mtp.ino
+++ examples/storage/mtp/mtp.ino
@@ -5,43 +5,52 @@
 #include <usb_drivers.h>
 
 // SET TEENSY USB TYPE TO "Serial + MTP Disk"
+// LED ON = USB drive is mounted, should be accessible via MTP
+// LED OFF = USB drive not mounted
+
+#define MTP_HANDLE_NONE 0xFFFFFFFF
 
 static DMAMEM TeensyUSBHost2 usb;
 static USB_FAT_Volume USBVol;
-static uint32_t mtp_handle = 0xFFFFFFFF;
+static uint32_t mtp_handle;
 
-void setup() {
+FLASHMEM void setup() {
   Serial.begin(0);
-  elapsedMillis stimer;
-  while (!Serial);
+  for (elapsedMillis t = 0; t < 5000;) {
+    if (Serial) break;
+  }
 
   if (CrashReport) CrashReport.printTo(Serial);
 
   usb.begin();
   USBVol.begin();
   MTP.begin();
-  MTP.loop(); // get rid of thread-unsafe interval timer _immediately_
+  MTP.loop(); // get rid of thread-unsafe interval timer ASAP
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
   // store the index in memory, not on a filesystem that could go away at any moment
   MTP.useFileSystemIndexFileStore(MTPStorage::INDEX_STORE_MEM_FILE);
+  mtp_handle = MTP_HANDLE_NONE;
 
-  printf("MTP START");
+  Serial.println("MTP START");
 }
 
 void loop() {
   if (USBVol.mediaPresent() == false) {
     digitalWrite(LED_BUILTIN, LOW);
-    if (mtp_handle != 0xFFFFFFFF) {
+    if (mtp_handle != MTP_HANDLE_NONE) {
       MTP.send_StoreRemovedEvent(mtp_handle);
       MTP.storage()->removeFilesystem(mtp_handle);
-      mtp_handle = 0xFFFFFFFF;
+      mtp_handle = MTP_HANDLE_NONE;
     }
 
+    // try to mount something
     if (USBVol.mount()) {
       digitalWrite(LED_BUILTIN, HIGH);
       mtp_handle = MTP.addFilesystem(USBVol, "USB Drive");
+      if (mtp_handle == MTP_HANDLE_NONE)
+        Serial.println("Failed to add new filesystem to MTP");
     }
   }
 
