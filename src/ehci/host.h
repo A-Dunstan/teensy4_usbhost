ee03269a63f96d887594e3dea1246ce2151f37f2
Andrew Dunstan <a_dunstan@hotmail.com>
2024-06-13 13:48:43 +1000

    add licence to core files

--- src/ehci/host.h
+++ src/ehci/host.h
@@ -1,5 +1,25 @@
+/*
+  Copyright (C) 2024 Andrew Dunstan
+  This file is part of teensy4_usbhost.
+
+  teensy4_usbhost is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _USB_HOST_H
 #define _USB_HOST_H
+
+#include "spec.h"
 
 #include <map>
 #include <vector>
@@ -7,8 +27,6 @@
 #include <atomic>
 #include <functional>
 #include <pgmspace.h>
-
-#include "spec.h"
 
 #if 1
 #define dprintf(fmt, ...) ::printf(PSTR(fmt) __VA_OPT__(,) __VA_ARGS__)
